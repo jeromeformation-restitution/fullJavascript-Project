9f114b23875f362f5ba8803fd3e9b186210203fa
makan <makanfainke78@gmail.com>
2019-08-19 14:38:47 +0200

    Model User

---
+++ src/app/Model/user.ts
@@ -0,0 +1,22 @@
+export class User {
+
+  public id: string;
+  public username: string;
+  public email: string;
+  public password: string;
+  public CreatedAt: Date;
+  public firstName: string;
+  public lastName: string;
+  public role: Array<string>;
+  public siret: number;
+  public adresse: string;
+  public ville: string;
+
+  constructor(id?: string, username?: string) {
+    this.id = id;
+    this.username = username;
+    this.siret = 0;
+  }
+
+
+}
