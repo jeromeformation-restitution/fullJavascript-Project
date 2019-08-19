52a4f89a3e2df138b16c38e2e04d1ba648f99299
thibmat <t.verhaeghe@free.fr>
2019-08-19 12:12:03 +0200

    initialisation back

--- api/app.js
+++ api/app.js
@@ -3,11 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//connexion a la base de données
+let mongoose = require('mongoose');
+console.log('Mongoose version : ' + mongoose.version);
+
+// Connexion à mongo
+mongoose.connect('mongodb://localhost/prestataires', {useNewUrlParser:true})
+  .then(
+    () => console.log('Connexion a mongo reussie !'),
+    (err) => {
+      throw new Error(err.message)
+    }
+  );
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
