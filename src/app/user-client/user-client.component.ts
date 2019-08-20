68b059549ed741dc0b2b53a7feefd323454db1fc
unknown <diedhiou.pyran@gmail.com>
2019-08-20 11:11:23 +0200

    creation de formulaire client

---
+++ src/app/user-client/user-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-client',
+  templateUrl: './user-client.component.html',
+  styleUrls: ['./user-client.component.scss']
+})
+export class UserClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
